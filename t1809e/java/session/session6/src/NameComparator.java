1779a6f3b22fa4cb428da5fcb71e9f24c724d290
hocgia <46387817+hocgia@users.noreply.github.com>
2019-05-07 09:36:34 +0700

    Delete NameComparator.java

--- t1809e/java/session/session6/src/NameComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class NameComparator implements Comparator<PhoneNumber> {
-
-    @Override
-    public int compare(PhoneNumber o1, PhoneNumber o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-}
