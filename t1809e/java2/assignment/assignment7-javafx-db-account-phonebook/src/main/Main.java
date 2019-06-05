744269dd0a88aef8ec305aaf97f20dd820d78138
hocgia <46387817+hocgia@users.noreply.github.com>
2019-06-05 20:45:24 +0700

    delete test
    delete test

--- t1809e/java2/assignment/assignment7-javafx-db-account-phonebook/src/main/Main.java
+++ t1809e/java2/assignment/assignment7-javafx-db-account-phonebook/src/main/Main.java
@@ -26,30 +26,8 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
-
+    
     public static void main(String[] args) {
         launch(args);
     }
-
-//    public static void main(String[] args) {
-//        PhonebookModel phonebookModel = new PhonebookModel();
-//
-//        try {
-//            phonebookModel.create(6, "6666", "1");
-//            phonebookModel.update(2, 3, "3333333", "2");
-//            List<Phonebook> phonebookList = phonebookModel.read();
-//            for (Phonebook phonebook : phonebookList) {
-//                System.out.println(phonebook.getTelephone());
-//            }
-//            phonebookModel.delete(2);
-//
-//            List<Phonebook> accountList = SingletonModel.findTelephone("trung9");
-//            for (Phonebook phonebook : accountList
-//            ) {
-//                System.out.println(phonebook.getTelephone());
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
