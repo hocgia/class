c96331ad3a4808539375b1f853e75ae4ffc736ba
hocgia <46387817+hocgia@users.noreply.github.com>
2019-08-02 20:42:41 +0700

    lab-session-6-countdown
    lab-session-6-countdown

---
+++ t1809e/c#/Lab-Session-6-Countdown/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new System.Threading.Thread(Countdown);
+            thread.Start();
+        }
+
+        public static void Countdown()
+        {
+            Console.WriteLine("{0,2:00}:{1,2:00}", 10, 00);
+            System.Threading.Thread.Sleep(1000);
+            for (var i = 9; i >= 0; i--)
+            {
+                for (var j = 59; j >= 0; j--)
+                {
+                    Console.WriteLine("{0,2:00}:{1,2:00}", i, j);
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+
+            Console.WriteLine("Time out.");
+        }
+    }
+}
